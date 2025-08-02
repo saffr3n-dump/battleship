6d4237afedda191a657cc9b9f776e805dbdb0f77
saffr3n <saffr3n@gmail.com>
2025-08-02 17:42:11 +0300

    feat: add GameBoard.at() method

--- src/game-board.js
+++ src/game-board.js
@@ -1,2 +1,39 @@
 export default class GameBoard {
+  #GRID_SIZE = 10;
+  #grid;
+
+  constructor() {
+    this.#grid = Array.from({ length: this.#GRID_SIZE }, () =>
+      Array.from({ length: this.#GRID_SIZE }, () => new Cell()),
+    );
+  }
+
+  at(x, y) {
+    if (!this.#isPositionValid([x, y])) {
+      throw new Error('Position is invalid');
+    }
+    return this.#grid[x][y];
+  }
+
+  #isPositionValid([x, y]) {
+    return (
+      typeof x === 'number' &&
+      typeof y === 'number' &&
+      Number.isInteger(x) &&
+      Number.isInteger(y) &&
+      x >= 0 &&
+      y >= 0 &&
+      x < this.#GRID_SIZE &&
+      y < this.#GRID_SIZE
+    );
+  }
+
+class Cell {
+  constructor() {
+    this.ship = null;
+  }
+
+  placeShip(ship) {
+    this.ship = ship;
+  }
 }
--- tests/game-board.test.js
+++ tests/game-board.test.js
@@ -4,4 +4,27 @@ describe('GameBoard', () => {
   it('is constructible', () => {
     expect(() => new GameBoard()).not.toThrow();
   });
+
+  it("has 'at()' method", () => {
+    expect(GameBoard.prototype.at).toBeDefined();
+  });
+
+  it("accepts '0,0' to '9,9' coordinates in 'at()'", () => {
+    const gameBoard = new GameBoard();
+    expect(() => gameBoard.at(-1, 0)).toThrow();
+    expect(() => gameBoard.at(0, 10)).toThrow();
+    expect(() => gameBoard.at('0', 0)).toThrow();
+    expect(() => gameBoard.at(0, 0.5)).toThrow();
+    expect(() => gameBoard.at(0, 0)).not.toThrow();
+    expect(() => gameBoard.at(9, 9)).not.toThrow();
+  });
+
+  it("has 'ship' property in each cell", () => {
+    const gameBoard = new GameBoard();
+    for (let i = 0; i < 10; ++i) {
+      for (let j = 0; j < 10; ++j) {
+        expect(gameBoard.at(i, j).ship).toBeDefined();
+      }
+    }
+  });
 });
