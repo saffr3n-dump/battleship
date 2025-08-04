8e698ee9b976374f3a0f51f363c548856b422b32
saffr3n <saffr3n@gmail.com>
2025-08-04 19:14:35 +0300

    feat: implement the main game loop

--- src/dom/enemy-board.js
+++ src/dom/enemy-board.js
@@ -3,6 +3,7 @@ import { GRID_SIZE } from '../constants.js';
 export default class EnemyBoard {
   static container = document.querySelector('.enemy .board');
   static cells = new Map();
+  static attackListeners = new Set();
   static board;
 
   static init(enemy) {
@@ -16,5 +17,85 @@ export default class EnemyBoard {
       }
     }
     this.board = enemy.gameBoard;
+    this.enable();
+  }
+
+  static enable() {
+    this.container.addEventListener('click', this.#onClick);
+  }
+
+  static disable() {
+    this.container.removeEventListener('click', this.#onClick);
+  }
+
+  static onAttack(callback) {
+    this.attackListeners.add(callback);
+  }
+
+  static #onClick = (e) => {
+    const cell = e.target;
+    if (
+      !cell.classList.contains('cell') ||
+      cell.classList.contains('disabled')
+    ) {
+      return;
+    }
+    this.disable();
+
+    const [x, y] = JSON.parse(cell.dataset.pos);
+    this.board.receiveAttack(x, y);
+    cell.classList.add('hit', 'disabled');
+
+    const { ship } = this.board.at(x, y);
+    if (ship) {
+      cell.classList.add('ship');
+      if (ship.length === 1) {
+        this.#disableNeighbors(x, y);
+      } else {
+        this.#disableDiagonals(x, y);
+        if (ship.isSunk()) this.#disableEnds(ship);
+      }
+
+      if (this.board.isEveryShipSunk()) {
+        setTimeout(() => alert('You win!'), 100);
+      } else {
+        this.enable();
+      }
+
+      return;
+    }
+
+    let gameOver;
+    for (const cb of this.attackListeners) gameOver ||= cb();
+    if (!gameOver) this.enable();
+  };
+
+  static #disableNeighbors(x, y) {
+    for (let i = -1; i <= 1; ++i) {
+      for (let j = -1; j <= 1; ++j) {
+        if (i === 0 && j === 0) continue;
+        const cell = this.cells.get(`${x + j},${y + i}`);
+        if (cell) cell.classList.add('disabled');
+      }
+    }
+  }
+
+  static #disableDiagonals(x, y) {
+    for (let i = -1; i <= 1; i += 2) {
+      for (let j = -1; j <= 1; j += 2) {
+        const cell = this.cells.get(`${x + j},${y + i}`);
+        if (cell) cell.classList.add('disabled');
+      }
+    }
+  }
+
+  static #disableEnds(ship) {
+    let [startX, startY] = ship.coordinates[0];
+    let [endX, endY] = ship.coordinates.at(-1);
+    ship.orientation === 'x' ? (--startX, ++endX) : (--startY, ++endY);
+    const startCell = this.cells.get(`${startX},${startY}`);
+    const endCell = this.cells.get(`${endX},${endY}`);
+    if (startCell) startCell.classList.add('disabled');
+    if (endCell) endCell.classList.add('disabled');
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Ship from './core/ship.js';
 import Player from './core/player.js';
 import PlayerBoard from './dom/player-board.js';
 import EnemyBoard from './dom/enemy-board.js';
+import { GRID_SIZE } from './constants.js';
 
 document.querySelector('button').onclick = () => window.location.reload();
 
@@ -36,3 +37,25 @@ const enemy = new Player();
 
 PlayerBoard.init(player);
 EnemyBoard.init(enemy);
+EnemyBoard.onAttack(() => {
+  while (true) {
+    const [x, y] = [
+      Math.floor(Math.random() * GRID_SIZE),
+      Math.floor(Math.random() * GRID_SIZE),
+    ];
+
+    const cell = PlayerBoard.cells.get(`${x},${y}`);
+    if (cell.classList.contains('disabled')) {
+      continue;
+    }
+
+    player.gameBoard.receiveAttack(x, y);
+    cell.classList.add('hit', 'disabled');
+    if (!player.gameBoard.at(x, y).ship) break;
+
+    if (player.gameBoard.isEveryShipSunk()) {
+      setTimeout(() => alert('You lose!'), 100);
+      return true;
+    }
+  }
+});
